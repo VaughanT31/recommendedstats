20f86de0794bdd2fd8466b05624e738d4381dcf0
VaughanThomas <vaughan.freelancedirect@gmail.com>
2026-08-27 07:08:33 +0200

    2026-08-27

--- RecommendedStats/Data/Meta.lua
+++ RecommendedStats/Data/Meta.lua
@@ -1,7 +1,7 @@
--- GENERATED 2026-08-26 - do not hand-edit
+-- GENERATED 2026-08-27 - do not hand-edit
 RecommendedStatsData_Meta = {
     ["schema"] = 1,
-    ["updated"] = "2026-08-26",
+    ["updated"] = "2026-08-27",
     ["gamePatch"] = "12.1.0",
     ["sampleSize"] = 20,
     ["source"] = "raider.io + Battle.net API",
