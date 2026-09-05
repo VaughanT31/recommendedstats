6e7ae35029c19ac2fd1dedfe213fe067c52255d0
VaughanThomas <vaughan.freelancedirect@gmail.com>
2026-09-05 08:55:50 +0200

    Updated dataset to 2026-09-05

--- RecommendedStats/Data/Meta.lua
+++ RecommendedStats/Data/Meta.lua
@@ -1,7 +1,7 @@
--- GENERATED 2026-09-04 - do not hand-edit
+-- GENERATED 2026-09-05 - do not hand-edit
 RecommendedStatsData_Meta = {
     ["schema"] = 1,
-    ["updated"] = "2026-09-04",
+    ["updated"] = "2026-09-05",
     ["gamePatch"] = "12.1.0",
     ["sampleSize"] = 20,
     ["source"] = "raider.io + Battle.net API",
--- RecommendedStats/Data/TierSet.lua
+++ RecommendedStats/Data/TierSet.lua
@@ -1,2 +1,2 @@
--- GENERATED 2026-09-04 - do not hand-edit
+-- GENERATED 2026-09-05 - do not hand-edit
 RecommendedStatsData_TierSet = {}
